cd049664fe6ed23dd10eedc32843449f04af68bc
Cristina <cristinae.uni@gmail.com>
2017-08-02 23:10:19 +0200

    script to calculate the coverage with common items

---
+++ scripts/textCoverage.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Counts the number of words in a file specified in another file
+(coverage of an input file by a list of words)
+"""
+
+import sys
+import os
+import re
+from collections import Counter
+
+def readDictionary(inputFile):
+    freqs = {}
+    with open(inputFile) as f:
+        for line in f:
+           (key, val) = line.split()
+           freqs[int(key)] = val
+    return freqs
+
+
+def getCommonFileName(inputFile):
+    #ex: train.en-it.it.wplmb.lema
+    m = re.search('(\w+\.\w\w-\w\w.)\w\w.wplmb\.(\w+)', inputFile)
+    if m:
+       prefix = m.group(1)
+       extension = m.group(2)
+    return prefix+extension
+
+
+def getLanguagePair(inputFile):
+    #ex: train.en-it.it.wplmb.lema
+    m = re.search('\w+\.(\w\w-\w\w).\w\w.wplmb\.\w+', inputFile)
+    if m:
+       pair = m.group(1)
+    return pair
+
+
+def getLanguage(inputFile):
+    #ex: train.en-it.it.wplmb.lema
+    m = re.search('\w+\.\w\w-\w\w.(\w\w).wplmb\.\w+', inputFile)
+    if m:
+       lang = m.group(1)
+    return lang
+
+
+def main(inputFile):
+
+    with open(inputFile) as f:
+	words = f.read().split()
+        wordcount = Counter(words)
+
+    with open(getCommonFileName(inputFile)) as f:
+	commonWords=0
+        for line in f:
+  	     commonWords = commonWords + wordcount[line.rstrip()]
+    #for item in wordcount.items(): print("{}\t{}".format(*item))
+
+    percentage = float(commonWords)/float(len(words))
+    print("{}.{}: {} out of {} (a {:.1%})".format(getLanguagePair(inputFile),getLanguage(inputFile),commonWords,len(words),percentage)) 
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) is not 2:
+        sys.stderr.write('Usage: python %s train \n' % sys.argv[0])
+        sys.exit(1)
+    main(sys.argv[1])
